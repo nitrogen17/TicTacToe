999b74b0d273b7ab92762253c77538d883afd2ea
Jonorsky Navarrete <jonorskydev@gmail.com>
2022-06-02 16:38:52 +0800

    Setup Board

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var board: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupBoard()
     }
 
-
+    private func setupBoard() {
+        guard let viewShadow = board else { return }
+        viewShadow.center = self.view.center
+        viewShadow.backgroundColor = UIColor.black
+        viewShadow.layer.shadowColor = UIColor.gray.cgColor
+        viewShadow.layer.shadowOpacity = 1
+        viewShadow.layer.shadowOffset = CGSize.zero
+        viewShadow.layer.shadowRadius = 5
+    }
 }
 
